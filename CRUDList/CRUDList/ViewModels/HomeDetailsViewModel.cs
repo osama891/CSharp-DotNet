7688c774a3957ac3a886a65491726e11616c60ca
Osama Sohail <62395943+osama891@users.noreply.github.com>
2020-04-29 04:01:15 +0500

    Add files via upload

---
+++ CRUDList/CRUDList/ViewModels/HomeDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CRUDList.Models;
+
+namespace CRUDList.ViewModels
+{
+    public class HomeDetailsViewModel
+    {
+       
+        public Task task { get; set; }
+        public string title { get; set; }
+
+    }
+
+    }
+      
+    
+
